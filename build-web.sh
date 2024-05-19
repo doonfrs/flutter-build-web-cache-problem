fa513d35339e9e19aa71910fbe28d122ac470c02
Feras Abdalrahman <doonfrs@gmail.com>
2024-05-19 21:48:27 +0300

    support flutter 3.22

--- build-web.sh
+++ build-web.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-echo incrementing build number
+dos2unix pubspec.yaml
+echo "incrementing build version..."
 perl -i -pe 's/^(version:\s+\d+\.\d+\.)(\d+)\+(\d+)$/$1.($2+1)."+".($3+1)/e' pubspec.yaml
 
 flutter clean
 flutter packages get
-flutter build web --release --web-renderer=html --pwa-strategy=none
+flutter build web --release --web-renderer=canvaskit --pwa-strategy=none #--no-tree-shake-icons #--source-maps
 
-echo "" > build/web/assets/NOTICES
 
 #replace base href
 echo Updating base href
-path="/"
-sed -i "s|<base href=\"/\">|<base href=\"$path\">|g" build/web/index.html
+baseHref="/"
+
+sed -i "s|<base href=\"/\">|<base href=\"$baseHref\">|g" build/web/index.html
 
 echo "reading version from pubspec.yaml without + sign"
 version=$(grep version: pubspec.yaml | sed 's/version: //g' | sed 's/+//g')
 
 echo "Patching version in js partial urls in main.dart.js"
-sed -i 's/\.createScriptURL([[:graph:]]\++[[:space:]]*\+[[:graph:]]/&+"?v='"$version"'"/g' build/web/main.dart.js
+sed -i "s/\`\${w}\${s}\`/\`\${w}\${s}?v=$version\`/g" build/web/flutter.js
+sed -i "s/\`\${w}\${s}\`/\`\${w}\${s}?v=$version\`/g" build/web/flutter_bootstrap.js
+sed -i "s/\`\${w}\${s}\`/\`\${w}\${s}?v=$version\`/g" build/web/index.html
 
-echo "Patching main.dart.js path in flutter.js"
-sed -i 's|\(${baseUri}main\.dart\.js\)|\1?v='"$version"'|g' build/web/flutter.js
 
-echo "Patching index.html for flutter.js"
-sed -i 's/"flutter\.js"/"flutter.js?v='"$version"'"/' build/web/index.html
+echo "Patching assets loader with v=$version in main.dart.js"
+sed -i "s/self\.window\.fetch(a),/self.window.fetch(a + '?v=$version'),/g" build/web/main.dart.js
+echo "Adding v= to manifest.json"
+sed -i 's/"manifest.json"/"manifest.json?v='"$version"'"/' build/web/index.html
+
